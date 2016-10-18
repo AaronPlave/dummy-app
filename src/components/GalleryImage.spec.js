eb9ec275c33a65094567c09f2026526c2bfe06a5
AaronPlave <aplave@wesleyan.edu>
2016-10-17 23:37:53 -0700

    Added in sinon test for GalleryImage

--- src/components/GalleryImage.spec.js
+++ src/components/GalleryImage.spec.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import { expect } from 'chai';
+import chai, { expect } from 'chai';
+import sinon from 'sinon';
+import sinonChai from 'sinon-chai';
 import GalleryImage from './GalleryImage';
 import { Palette } from './Palette';
 import AsyncImage from './AsyncImage'; // eslint-disable-line import/no-named-as-default
+
+chai.use(sinonChai);
 
 describe('<GalleryImage />', () => {
     it('should contain <AsyncImage />', () => {
@@ -30,7 +34,17 @@ describe('<GalleryImage />', () => {
 
     it('should have a <Palette />', () => {
         const wrapper = shallow(<GalleryImage src="?" palette={[]} onImageLoad={()=>{}} id="?"/>);
-        
         expect(wrapper.find(Palette)).to.be.length(1);
+    });
+
+    it('should call passed onImageLoad function with correct parameters', () => {
+        const onImageLoad = sinon.stub().returns("el");
+        const wrapper = shallow(<GalleryImage src="!" palette={[]} onImageLoad={onImageLoad} id="?"/>);
+        
+        expect(onImageLoad.calledOnce).to.be.false; // Make sure it has never been called
+        wrapper.instance().props["onImageLoad"]("el"); // Get the prop from instance, call it with param
+        expect(wrapper.find(Palette)).to.be.length(1); 
+        expect(onImageLoad.calledOnce).to.be.true; // Make sure it has been called once
+        expect(onImageLoad).to.have.been.calledWith(onImageLoad()); // Make sure it has been called with the correct function
     });
 });
