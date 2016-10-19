aea841cc3bf1d19adf44b9da48a3d040989ea665
AaronPlave <aplave@wesleyan.edu>
2016-10-19 00:44:15 -0700

    Disabling git diff check in deploy.

--- deploy.sh
+++ deploy.sh
@@ -55,10 +55,10 @@ git config user.email "$COMMIT_AUTHOR_EMAIL"
 
 # If there are no changes to the compiled out (e.g. this is a README update) then just bail.
 # if [ -z `git diff --exit-code` ]; then
-if git diff --quiet ; then
-    echo "No changes to the output on this push; exiting."
-    exit 0
-fi
+# if git diff --quiet ; then
+#     echo "No changes to the output on this push; exiting."
+#     exit 0
+# fi
 
 # Add CNAME for deploy
 echo "dummy-app.aaronplave.com" > CNAME
