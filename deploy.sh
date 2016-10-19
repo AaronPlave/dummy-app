6cf455b69ac4bd27c88bb73839b366ddcdcf2e40
AaronPlave <aplave@wesleyan.edu>
2016-10-18 22:31:09 -0700

    More deploy fix attempt, this time with more logs.

--- deploy.sh
+++ deploy.sh
@@ -40,17 +40,20 @@ git config user.email "$COMMIT_AUTHOR_EMAIL"
 
 # If there are no changes to the compiled out (e.g. this is a README update) then just bail.
 # if [ -z `git diff --exit-code` ]; then
-# if git diff --quiet ; then
-#     echo "No changes to the output on this push; exiting."
-#     exit 0
-# fi
+if git diff --quiet ; then
+    echo "No changes to the output on this push; exiting."
+    exit 0
+fi
 
 ls
+git status
 
 # Commit the "changes", i.e. the new version.
 # The delta will show diffs between new and old versions.
 git add .
+git status
 git commit -m "Deploy to GitHub Pages: ${SHA}"
+git status
 
 # Get the deploy key by using Travis's stored variables to decrypt deploy_key.enc
 ENCRYPTED_KEY_VAR="encrypted_${ENCRYPTION_LABEL}_key"
