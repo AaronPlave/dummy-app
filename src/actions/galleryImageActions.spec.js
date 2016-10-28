86d9afff705e580beade840c24110c8e3d1408c3
AaronPlave <aplave@wesleyan.edu>
2016-10-28 10:38:01 -0700

    Pruning more starter kit files, adding in more test data, working on testing.

--- src/actions/galleryImageActions.spec.js
+++ src/actions/galleryImageActions.spec.js
@@ -8,6 +8,40 @@ import chai, { expect } from 'chai';
 chai.use(sinonChai);
 
 describe('Actions', () => {
+    it('should create an action to load more images', () => {
+        const actual = galleryImageActions.loadMoreImages();
+        const expected = { type: actionTypes.LOAD_MORE_IMAGES };
+
+        expect(actual).to.deep.equal(expected); // Notice use of deep because it's a nested object
+    });
+
+    let xhr;
+    // let model;
+    // let options;
+    let requests;
+
+
+    beforeEach(() => {
+        xhr = sinon.useFakeXMLHttpRequest();
+        global.XMLHttpRequest = xhr;
+        requests = [];
+        /* eslint no-shadow: 1*/
+        xhr.onCreate = (xhr) => {
+            requests.push(xhr);
+        };
+        // model = { id: 42 };
+        // model.toJSON = () => {
+        //     return model;
+        // };
+        // options = {};
+        // options.success = expect.createSpy();
+        // options.error = expect.createSpy();
+    });
+
+    afterEach(() => {
+        xhr.restore();
+    });
+
     it('should create an action to calculate a color palette', () => {
         const dispatch = sinon.spy();
         const expected = {
@@ -15,24 +49,19 @@ describe('Actions', () => {
             url: "test.com",
             id: "123"
         };
+        // console.log("blob", new Blob)
 
         // we expect this to return a function since it is a thunk
         expect(typeof(galleryImageActions.calculateColorPalette(expected.url, expected.id))).to.equal('function');
         // then we simulate calling it with dispatch as the store would do
         expect(galleryImageActions.calculateColorPalette(expected.url, expected.id)(dispatch)).to.be.a('promise');
+        // requests[0].respond(200, { 'Content-Type': 'application/octet-stream' }, 'Ok');
         // expect(dispatch).to.have.been.calledWith(expected);
         // console.log(galleryImageActions.calculateColorPalette(expected.url, expected.id)(dispatch),"DOS{")
-        // galleryImageActions.calculateColorPalette(expected.url, expected.id)(dispatch).then(result => {
+        // return galleryImageActions.calculateColorPalette(expected.url, expected.id)(dispatch).then(result => {
         //     console.log("DONE", r)
         //     expect(dispatch).to.have.been.calledWith(expected);
         //     // done();
         // })
-    });
-
-    it('should create an action to load more images', () => {
-        const actual = galleryImageActions.loadMoreImages();
-        const expected = { type: actionTypes.LOAD_MORE_IMAGES };
-
-        expect(actual).to.deep.equal(expected); // Notice use of deep because it's a nested object
     });
 });
--- src/constants/appConfig.js
+++ src/constants/appConfig.js
@@ -1,2 +1,2 @@
-export const GALLERY_PAGE_IMAGES_PER_PAGE = 3;
+export const GALLERY_PAGE_IMAGES_PER_PAGE = 5;
 export const NUM_PALETTE_COLORS = 5;
